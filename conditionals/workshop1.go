cc983dbd79b24b56bcf26566f217bbcfe0c66135
berrua <berru14a@gmail.com>
2023-10-24 14:24:36 +0300

    GO101

--- conditionals/workshop1.go
+++ conditionals/workshop1.go
@@ -5,12 +5,15 @@ import "fmt"
 func Workshop1() {
 	// üç adet sayıdan en büyüğünü ekrana yazdır
 	var sayi1, sayi2, sayi3 int = 40, 30, 50
+	var enBuyuk int = sayi1
 
-	if sayi1 > sayi2 && sayi1 > sayi3 {
-		fmt.Printf("En büyük sayı: %v", sayi1)
-	} else if sayi2 > sayi1 && sayi2 > sayi3 {
-		fmt.Printf("En büyük sayı: %v", sayi2)
-	} else {
-		fmt.Printf("En büyük sayı: %v", sayi3)
+	if sayi2 > enBuyuk {
+		enBuyuk = sayi2
 	}
+
+	if sayi3 > enBuyuk {
+		enBuyuk = sayi3
+	}
+
+	fmt.Printf("En büyük sayı: %v", enBuyuk)
 }
