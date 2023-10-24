50081d2d1292cc4e785268a981d664027421a3f6
berrua <berru14a@gmail.com>
2023-10-24 14:42:14 +0300

    GO101

---
+++ loops/for-demo1.go
@@ -0,0 +1,7 @@
+package loops
+
+import "fmt"
+
+func Demo1() {
+	fmt.Printf("loops.")
+}
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	//"fmt"
 	//"goexamples/variables"
-	"goexamples/conditionals"
+	//"goexamples/conditionals"
+	"goexamples/loops"
 )
 
 func main() {
@@ -11,5 +12,6 @@ func main() {
 	//variables.Demo1()
 	//conditionals.Demo1()
 	//conditionals.Demo2()
-	conditionals.Workshop1()
+	//conditionals.Workshop1()
+	loops.Demo1()
 }
