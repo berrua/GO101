35ba4dc4dd60bb5983443ab6019d085a5b5c3224
berrua <berru14a@gmail.com>
2023-10-26 10:34:22 +0300

    GO101

--- loops/workshop3.go
+++ loops/workshop3.go
@@ -4,20 +4,9 @@ import "fmt"
 
 func Workshop3() {
 	//asal sayı mı
-	sayi := 80
-	tahmin := 0
+	sayi := 0
 
 	fmt.Println("Bir sayı giriniz...")
-	fmt.Scanln(&tahmin)
+	fmt.Scanln(&sayi)
 
-	for sayi != tahmin {
-		if tahmin < sayi {
-			fmt.Println("Daha büyük bir sayı tahmin ediniz.")
-			fmt.Scanln(&tahmin)
-		} else if tahmin > sayi {
-			fmt.Println("Daha küçük bir sayı tahmin ediniz.")
-			fmt.Scanln(&tahmin)
-		}
-	}
-	fmt.Println("Tebrikler!")
 }
